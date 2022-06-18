31c4d7fc8652a9908dcca30b5e6618e14e4f627b
Matheus <mvbturatti@gmail.com>
2022-06-18 17:15:52 -0300

    changes to add prob

--- alternante/destinatario/apps/server/lib/state_machine.ex
+++ alternante/destinatario/apps/server/lib/state_machine.ex
@@ -11,17 +11,26 @@ defmodule StateMachine do
 
   defp read_line(socket) do
     case :gen_tcp.recv(socket, 32, 10000) do
-      {:ok, packet} ->  parse_packet(packet)
+      {:ok, packet} ->  check_prob(packet)
       {:error, _reason} -> "something went wrong"
+    end
+  end
+
+  defp check_prob(packet) do
+    prob = String.to_integer(System.get_env("PROB") || "100")
+    prob = prob |> min(100) |> max(0)
+
+    num = Enum.random(1..100)
+    cond do
+      num <= prob ->
+        parse_packet(packet)
+      num > prob ->
+        "something went wrong"
     end
   end
 
   defp parse_packet(<<seqnun::binary-size(4), acknum::binary-size(4), checksum::binary-size(4), payload::binary>> = packet) do
     IO.puts("seqnum: #{seqnun} \nacknum: #{acknum}\nchecksum: #{checksum}\npayload:#{payload}")
-    packet
-  end
-  defp parse_packet(packet) do
-    IO.puts("entrou aq")
     packet
   end
 
