b28db0d0cfec6c19762fafc2771cf707df94dc1a
Matheus <mvbturatti@gmail.com>
2022-06-20 19:17:23 -0300

    solution to alternating bit

--- alternante/destinatario/apps/server/lib/state_machine.ex
+++ alternante/destinatario/apps/server/lib/state_machine.ex
@@ -1,37 +1,49 @@
 defmodule StateMachine do
 
-
-  def serve(socket) do
-    socket
-    |> read_line()
-    |> write_line(socket)
-
-    serve(socket)
+  def serve(socket, ack \\ 0) do
+    {msg, ack} = read_line(socket, ack)
+    write_line(msg, socket)
+    IO.puts("End of message\nWaiting for new one!\n\n################################\n\n")
+    serve(socket, ack)
   end
 
-  defp read_line(socket) do
-    case :gen_tcp.recv(socket, 32, 10000) do
-      {:ok, packet} ->  check_prob(packet)
-      {:error, _reason} -> "something went wrong"
+  defp read_line(socket, ack) do
+    case :gen_tcp.recv(socket, 32) do
+      {:ok, packet} ->
+        case check_prob(packet, ack) do
+          {:prob, _} -> serve(socket, ack)
+          return -> return
+        end
+      {:error,_packet} -> :gen_tcp.close(socket)
     end
   end
 
-  defp check_prob(packet) do
+  defp check_prob(packet, ack) do
     prob = String.to_integer(System.get_env("PROB") || "100")
     prob = prob |> min(100) |> max(0)
 
     num = Enum.random(1..100)
     cond do
       num <= prob ->
-        parse_packet(packet)
+        parse_packet(packet, ack)
       num > prob ->
-        "something went wrong"
+        IO.puts("Packet missed by probability settings!")
+        IO.puts("Sending nothing\n\n==========================")
+        {:prob, ack}
     end
   end
-
-  defp parse_packet(<<seqnun::binary-size(4), acknum::binary-size(4), checksum::binary-size(4), payload::binary>> = packet) do
-    IO.puts("seqnum: #{seqnun} \nacknum: #{acknum}\nchecksum: #{checksum}\npayload:#{payload}")
-    packet
+  defp parse_packet(<<>>, _ack), do: {:error, "probability settings void message"}
+  defp parse_packet(<<seqnun::size(4)-unit(8), acknum::size(4)-unit(8), checksum::size(4)-unit(8), payload::binary>>, ack) do
+    IO.puts("Message received! \n\n")
+    IO.puts("sequence number: #{seqnun}\nACK: #{acknum}\nchecksum: #{checksum}\npayload:")
+    IO.inspect(payload)
+    cond do
+      acknum == ack ->
+        ack = (ack == 1 && 0) || 1  #muda entre 0 e 1, invertendo.
+        {<<acknum::size(4)-unit(8)>> <> <<checksum::size(4)-unit(8)>>, ack}
+      true ->
+        {<<0,0,0,ack>> <> <<0::size(4)-unit(8)>>, ack}
+    end
   end
 
   defp write_line(line, socket) do
--- alternante/remetente/lib/remetente.ex
+++ alternante/remetente/lib/remetente.ex
@@ -1,18 +1,69 @@
 defmodule Remetente do
-  @moduledoc """
-  Documentation for `Remetente`.
-  """
+  use GenStateMachine, callback_mode: [:handle_event_function, :state_enter]
 
-  @doc """
-  Hello world.
+  # Client
 
-  ## Examples
+  def start_link() do
+    {:ok, sock} = :gen_tcp.connect({127,0,0,1}, 4321, [:binary, active: false, packet: :raw])
+    GenStateMachine.start_link(Remetente, {:off, %{seqnum: 0, ack: 0, data: 0, socket: sock}})
+  end
 
-      iex> Remetente.hello()
-      :world
+  def flip(pid) do
+    GenStateMachine.cast(pid, :flip)
+  end
 
-  """
-  def hello do
-    :world
+  def get_count(pid) do
+    GenStateMachine.call(pid, :get_count)
+  end
+
+  # Server (callbacks)
+
+  def handle_event(:enter, _event, state, data) do
+    IO.puts("Dados atuais:\n")
+    IO.inspect(data)
+    {:next_state, state, data}
+  end
+
+  def handle_event(:cast, :flip, :off, data) do
+    check_message(data, :off)
+  end
+
+  def handle_event(:cast, :flip, :on, data) do
+    check_message(data, :on)
+  end
+
+  def handle_event({:call, from}, :get_count, state, data) do
+    {:next_state, state, data, [{:reply, from, data}]}
+  end
+
+  def handle_event(event_type, event_content, state, data) do
+    # Call the default implementation from GenStateMachine
+    super(event_type, event_content, state, data)
+  end
+
+  defp send_message(data) do
+    IO.puts("\n===============================\n\nEnvio:\n")
+    IO.puts("ACK: #{data.data}\n")
+    IO.puts("Dados:\n")
+    IO.inspect(data)
+    :gen_tcp.send(data.socket, <<data.seqnum::size(4)-unit(8), data.ack::size(4)-unit(8),0::size(4)-unit(8), data.data::size(20)-unit(8)>>)
+  end
+
+  defp check_message(data, state) do
+    send_message(data)
+    with {:ok, msg} <- :gen_tcp.recv(data.socket, 8, 1000)
+    do
+      IO.puts("Resposta recebida, ack recebido:\n")
+      <<d::size(4)-unit(8), _rest::binary>> = msg
+      IO.puts("ACK recebido: #{d}\n\n====================================")
+      case state do
+        :off -> {:next_state, :on, %{data | seqnum: data.seqnum + 20, data: data.data + 1, ack: 1}}
+        :on -> {:next_state, :off, %{data | seqnum: data.seqnum + 20, data: data.data + 1, ack: 0}}
+      end
+    else
+      {:error, _} ->
+        IO.puts("Timed out response. Retry.\n")
+        check_message(data,state)
+    end
   end
 end
